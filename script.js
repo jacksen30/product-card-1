13e60433ab5547c34afc07e7e1998b8af19c8e17
Jacksen Nillson <nillsonjacksen@gmail.com>
2023-10-11 23:15:01 +0800

    update css with safari prefixes

--- script.js
+++ script.js
@@ -1,28 +1,32 @@
-function flip() {
-    alert("Flip function was called.");
-    console.log("Flip function called.");
-
+document.addEventListener('DOMContentLoaded', () => {
     const card = document.querySelector('.card');
     const btnSecondary = document.querySelector('.button-card--secondary');
+    let isFlipped = false;
 
-    if (card.style.transform === "rotateY(180deg)") {
-        card.style.transform = '';
-        btnSecondary.style.transform = '';
-        btnSecondary.style.zIndex = "0";
-    } else {
-        card.style.transform = "rotateY(180deg)";
-        btnSecondary.style.transform = 'translate(-130px, 20px)';
-        btnSecondary.style.zIndex = "2";
+    function flip() {
+        alert("Flip function was called.");
+        console.log("Flip function called.");
+
+        isFlipped = !isFlipped; // Toggle flip state
+
+        if (isFlipped) {
+            card.style.transform = "rotateY(180deg)";
+            btnSecondary.style.transform = 'translate(-130px, 20px)';
+            btnSecondary.style.zIndex = "2";
+        } else {
+            card.style.transform = "";
+            btnSecondary.style.transform = '';
+            btnSecondary.style.zIndex = "0";
+        }
+
+        // Trigger a reflow to ensure transformations take effect
+        card.style.display = 'none';
+        card.offsetHeight;
+        card.style.display = '';
     }
 
-    // Trigger a reflow to ensure transformations take effect and potentially resolve rendering issues
-    card.style.display = 'none';
-    card.offsetHeight;
-    card.style.display = '';
-}
-
-// Ensure the DOM is fully loaded before attaching the event listener
-document.addEventListener('DOMContentLoaded', (event) => {
-    document.querySelector('.button-card--secondary').addEventListener('touchend', flip);
+    btnSecondary.addEventListener('touchend', flip);
+    btnSecondary.addEventListener('click', flip);
 });
+
 
