5b70b6f42d703cbe606a71ec5e8e5862072746e7
Jacksen Nillson <nillsonjacksen@gmail.com>
2023-10-11 22:19:38 +0800

    update javascript to add required positioning css inline only once card is flipped

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
 function flip() {
-    document.querySelector('.card').classList.toggle('flipped');
+    const card = document.querySelector('.card');
+    card.classList.toggle('flipped');
+
+    const btnSecondary = document.querySelector('.button-card--secondary');
+    if (card.classList.contains('flipped')) {
+        btnSecondary.style.transform = 'translate(-130px, 20px)';
+    } else {
+        btnSecondary.style.transform = '';
+    }
 }
