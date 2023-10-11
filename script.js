d096a995f9f8634c4c3278e01d9bc13cd91f79ea
Jacksen Nillson <nillsonjacksen@gmail.com>
2023-10-11 22:52:10 +0800

    add javascript repaint after flip

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 function flip() {
+    console.log("Flip function called.");  // This will log a message in the console every time the flip function is called
+
     const card = document.querySelector('.card');
     card.classList.toggle('flipped');
 
@@ -10,6 +12,14 @@ function flip() {
         btnSecondary.style.transform = '';
         btnSecondary.style.zIndex = "0";
     }
+
+    // Trigger a reflow to ensure transformations take effect and potentially resolve rendering issues
+    card.style.display = 'none';
+    card.offsetHeight; // This line triggers a reflow
+    card.style.display = '';
 }
 
-document.querySelector('.button-card--secondary').addEventListener('click', flip);
+// Ensure the DOM is fully loaded before attaching the event listener
+document.addEventListener('DOMContentLoaded', (event) => {
+    document.querySelector('.button-card--secondary').addEventListener('click', flip);
+});
